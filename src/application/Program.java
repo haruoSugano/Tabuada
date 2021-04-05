620239ed6b094842570e61dd3a4d82b25cfd07b3
Helio Haruo <haruosugano@icloud.com>
2021-04-04 21:18:19 -0300

    Tabuada

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,16 +2,17 @@ package application;
 
 import javax.swing.JOptionPane;
 
-import entities.Tabuada;
-
 public class Program {
 
 	public static void main(String[] args) {
 		
 		int x = Integer.parseInt(JOptionPane.showInputDialog("Tabuada de 0 a "));
+		
 		for(int n=0; n<x; n++) {
-		Tabuada tab = new Tabuada(n);
-		tab.Multiplica(n);
+			for(int i=0; i<10; i++) {
+			int m = n*i;
+			JOptionPane.showMessageDialog(null, "Tabuada de " + n + "\n"+ n + " x " + i + " =" + m);
+		}
 		}
 	}
 
